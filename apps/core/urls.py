f5b618bbe2030ed4b574ef63a94d217578e5dac4
Dennis <dequaderer@gmail.com>
2022-08-12 01:15:05 -0400

    Check drone medication

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import StatusAPIView, DroneRegisterAPIView, MedicationRegisterAPIView
+from .views import StatusAPIView, DroneRegisterAPIView, MedicationLoadAPIView, MedicationCheckAPIView
 
 urlpatterns = [
     path('drones-register', DroneRegisterAPIView.as_view()),
-    path('drones-load-medication', MedicationRegisterAPIView.as_view())
+    path('drones-load-medication', MedicationLoadAPIView.as_view()),
+    path('drones-check-medication/<str:drone_serial_number>',
+         MedicationCheckAPIView.as_view())
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from apps.core.models import Drone, Medication
 from .serializers import DroneSerializer, MedicationSerializer
 from .utils import can_load_medication_weight, add_drone_to_medications
@@ -16,7 +16,7 @@ class DroneRegisterAPIView(CreateAPIView):
     queryset = Drone.objects.all()
 
 
-class MedicationRegisterAPIView(APIView):
+class MedicationLoadAPIView(APIView):
     def post(self, request):
         drone_serial_number = request.data['drone']
         medications = request.data['medications']
@@ -32,3 +32,14 @@ class MedicationRegisterAPIView(APIView):
                 return Response(serializer.errors, status=400)
         else:
             return Response({"medications": f"sum of medications weight must be less than drone weight limit of: {drone.weight_limit}"}, status=400)
+
+
+class MedicationCheckAPIView(APIView):
+    def get(self, request, drone_serial_number):
+        try:
+            Drone.objects.get(serial_number=drone_serial_number)
+        except Drone.DoesNotExist:
+            return Response({'Drone': f'Drone with serial number {drone_serial_number} does not exist'}, status=404)
+        medications = Medication.objects.filter(drone=drone_serial_number)
+        serializer = MedicationSerializer(medications, many=True)
+        return Response(serializer.data, status=200)
