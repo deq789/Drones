1fc4c3d1fe0d22f57d8c62065a539b20d1c011a9
Dennis <dequaderer@gmail.com>
2022-08-14 12:53:11 -0400

    Prevent drone LOADING state if battery is below 25

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 from .models import Drone, Medication
 
 
@@ -6,6 +6,11 @@ class DroneSerializer(ModelSerializer):
     class Meta:
         model = Drone
         fields = '__all__'
+
+    def create(self, validated_data):
+        if self.validated_data['battery_percentage'] < 25 and self.validated_data['state'] == 'LOADING':
+            raise ValidationError({"detail": "Drone can not be in loading state with battery percentage below 25%"})
+        return super().create(validated_data)
 
 
 class MedicationSerializer(ModelSerializer):
