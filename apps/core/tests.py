623d63d195c8f18f5794d44e3aab761283fedbb0
Dennis <dequaderer@gmail.com>
2022-08-17 13:19:19 -0400

    Add tests for drones creation

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from apps.core.models import Medication, Drone
 
-# Create your tests here.
+
+class DroneTests(APITestCase):
+    def test_create_drone(self):
+        """
+        Ensure we can create a new drone object.
+        """
+        url = reverse('drones')
+        data = {
+            "serial_number": "serial-number-test-1",
+            "model": "Lightweight",
+            "weight_limit": "174",
+            "battery_percentage": 40,
+            "state": "LOADING"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Drone.objects.count(), 1)
+        self.assertEqual(Drone.objects.get().state, 'LOADING')
--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,8 +3,10 @@ from .views import StatusAPIView, DroneRegisterAPIView, MedicationLoadAPIView, \
     MedicationCheckAPIView, DroneDetailPIView
 
 urlpatterns = [
-    path('drones/', DroneRegisterAPIView.as_view()),
-    path('drones/<pk>/', DroneDetailPIView.as_view()),
-    path('drones/load-medication', MedicationLoadAPIView.as_view()),
-    path('drones/check-medication/<pk>/', MedicationCheckAPIView.as_view())
+    path('drones/', DroneRegisterAPIView.as_view(), name='drones'),
+    path('drones/<pk>/', DroneDetailPIView.as_view(), name='drones-detail'),
+    path('drones/load-medication', MedicationLoadAPIView.as_view(),
+         name='drones-load-medication'),
+    path('drones/check-medication/<pk>/', MedicationCheckAPIView.as_view(),
+         name='drones-check-medication')
 ]
